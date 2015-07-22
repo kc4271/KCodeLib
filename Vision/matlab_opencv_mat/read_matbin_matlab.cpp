80d085b2ed190325581c8ed499e4ea23af7a6cae
Yan Kong <kc4271@gmail.com>
2015-07-22 17:35:53 +0800

    opencv/matlab read write matbin

---
+++ Vision/matlab_opencv_mat/read_matbin_matlab.cpp
@@ -0,0 +1,162 @@
+#include "mex.h"
+#include "KIndexer.h"
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+struct CSubInd {
+    int dims;
+    std::vector<int> sizes;
+    std::vector<int> row_major_presum;
+    std::vector<int> col_major_presum;
+
+    void init(int dims_, int *sizes_) {
+        dims = dims_;
+        sizes.resize(dims_);
+
+        for (int idx = 0; idx < dims; idx++) {
+            sizes[idx] = sizes_[idx];
+        }
+
+        row_major_presum.resize(dims, 1);
+        col_major_presum.resize(dims, 1);
+
+        int base = 1;
+        for (int idx = dims - 1; idx >= 0; idx--) {
+            row_major_presum[idx] = base;
+            base *= sizes[idx];
+        }
+
+        base = 1;
+        for (int idx = 0; idx < dims; idx++) {
+            col_major_presum[idx] = base;
+            base *= sizes[idx];
+        }
+    }
+
+    // convert index to row-major sub
+    std::vector<int> ind2sub_row(int ind) {
+        std::vector<int> sub(dims, 0);
+        for (int jdx = dims - 1; jdx >= 0; jdx--) {
+            sub[jdx] = ind / row_major_presum[jdx] % sizes[jdx];
+            ind -= sub[jdx] * row_major_presum[jdx];
+        }
+        return sub;
+    }
+
+    // convert index to col-major sub
+    std::vector<int> ind2sub_col(int ind) {
+        std::vector<int> sub(dims, 0);
+        for (int jdx = 0; jdx < dims; jdx++) {
+            sub[jdx] = ind / col_major_presum[jdx] % sizes[jdx];
+            ind -= sub[jdx] * col_major_presum[jdx];
+        }
+        return sub;
+    }
+
+    // convert sub to row-major index
+    int sub2ind_row(std::vector<int> const &sub) {
+        int ind = 0;
+        for (int jdx = dims - 1; jdx >= 0; jdx--) {
+            ind += sub[jdx] * row_major_presum[jdx];
+        }
+        return ind;
+    }
+
+    // convert sub to col-major index
+    int sub2ind_col(std::vector<int> const &sub) {
+        int ind = 0;
+        for (int jdx = 0; jdx < dims; jdx++) {
+            ind += sub[jdx] * col_major_presum[jdx];
+        }
+        return ind;
+    }
+};
+
+void read_matbin(const char *path, mxArray* &res) {
+    mxClassID const matlab_class_mapper [] = { 
+        mxUINT8_CLASS, mxINT8_CLASS, 
+        mxUINT16_CLASS, mxINT16_CLASS,
+        mxUINT32_CLASS, mxSINGLE_CLASS, 
+        mxDOUBLE_CLASS
+    };
+    std::ifstream fin(path, std::ofstream::in | std::ofstream::binary);
+    if (fin.fail()) {
+        mexErrMsgIdAndTxt("MATLAB:read_matbin:invalidPath",
+            "Cannot read matbin file");
+    }
+
+    int type, dims;
+    fin.read((char *) &type, sizeof(int));
+    fin.read((char *) &dims, sizeof(int));
+
+    int depth = type & 7;
+    int elem_size = 1 << (depth / 2);
+
+    int *sizes = new int [dims + 1];
+    size_t matsize = 1;
+    for (int idx = 0; idx < dims; idx++) {
+        fin.read((char *) (sizes + idx), sizeof(int));
+        matsize *= sizes[idx];
+    }
+    
+    int channels = ((type & (511 << 3)) >> 3) + 1;
+    if (channels > 1) {
+        sizes[dims] = channels;
+        dims++;     
+    }
+
+    char *data = new char[elem_size * matsize * channels];
+    fin.read(data, elem_size * matsize * channels);
+    fin.close();
+
+    CSubInd si;
+    si.init(dims, &sizes[0]);
+
+    res = mxCreateNumericArray(dims, &sizes[0], matlab_class_mapper[depth], mxREAL);
+
+    char *dst_data = (char *)mxGetData(res);
+
+    size_t count = matsize * channels;
+    std::vector<int> sub(dims, 0);
+    for (int idx = 0; idx < count; idx++) {
+        std::vector<int> sub = si.ind2sub_row(idx);
+        int dst_idx = si.sub2ind_col(sub) * elem_size;
+        int src_idx = idx * elem_size;
+        for (int jdx = 0; jdx < elem_size; jdx++) {
+            dst_data[dst_idx + jdx] = data[src_idx + jdx];
+        }
+    }
+
+    delete []data;
+    delete []sizes;
+}
+
+void mexFunction(int nlhs, mxArray *plhs[],
+                 int nrhs, const mxArray *prhs[])
+{
+    if(1 != nrhs) {
+        mexPrintf("Usage: read_matbin(\'XXXX.matbin\')");
+        return;
+    }
+    
+    if(!mxIsChar(prhs[0])) {
+        mexErrMsgIdAndTxt( "MATLAB:read_matbin:invalidInputType",
+                    "Input must be of type char.");
+    }
+    
+    if(0 == nlhs) {
+        return;
+    }
+    
+    string matbin_path = mxArrayToString(prhs[0]);
+    ifstream fin(matbin_path, std::fstream::in | std::fstream::binary);
+    if(fin.fail()) {
+        mexErrMsgIdAndTxt( "MATLAB:read_matbin:invalidPath",
+                    "Cannot read matbin file");
+    }
+    
+    read_matbin(matbin_path.c_str(), plhs[0]);
+}
+
---
+++ Vision/matlab_opencv_mat/read_write_matbin_opencv.cpp
@@ -0,0 +1,67 @@
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cstdio>
+
+/*
+matbin format:
+TYPE DIM_NUM DIM1 DIM2 DIM3 ...
+DATA
+*/
+bool write_matbin(const char *path, cv::Mat const &dst) {
+    if (dst.empty()) return false;
+    
+    int elem_size = 1 << (dst.depth() / 2);
+
+    std::ofstream fout(path, std::ofstream::out | std::ofstream::binary);
+    if (!fout.fail()) {
+        int type = dst.type();
+        fout.write((char *) (&type), sizeof(int));
+
+        int dims = dst.size.p[-1];
+        fout.write((char *) (&dims), sizeof(int));
+
+        size_t matsize = 1;
+        for (int idx = 0; idx < dims; idx++) {
+            int len = dst.size.p[idx];
+            fout.write((char *) (&len), sizeof(int));
+            matsize *= len;
+        }
+
+        int channels = dst.channels();
+        fout.write((char *) dst.data, elem_size * matsize * channels);
+        fout.close();
+    }
+    else {
+        return false;
+    }
+}
+
+bool read_matbin(const char *path, cv::Mat &res) {
+    std::ifstream fin(path, std::ofstream::in | std::ofstream::binary);
+    if (fin.fail()) return false;
+
+    int type, dims;
+    fin.read((char *) &type, sizeof(int));
+    fin.read((char *) &dims, sizeof(int));
+
+    int depth = type & 7;
+    int elem_size = 1 << (depth / 2);
+
+    int *sizes = new int [dims];
+    size_t matsize = 1;
+    for (int idx = 0; idx < dims; idx++) {
+        fin.read((char *) (sizes + idx), sizeof(int));
+        matsize *= sizes[idx];
+    }
+    
+    int channels = ((type & (511 << 3)) >> 3) + 1;
+
+    res.create(dims, sizes, type);
+    fin.read((char *) res.data, elem_size * matsize * channels);
+    fin.close();
+    delete []sizes; 
+
+    return true;
+}
